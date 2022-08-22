abba5f9e97cd234d59202f92256635c1beb45571
Rai <raibr16@gmail.com>
2022-08-22 21:16:01 +0200

    Buttom fix

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "/gender_widget.dart";
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,6 +9,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _gender = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +18,17 @@ class _HomeScreenState extends State<HomeScreen> {
           centerTitle: true,
           title: const Text("BMI Calculator"),
         ),
-        body: Container());
+        body: SingleChildScrollView(
+            child: Container(
+                padding: const EdgeInsets.all(12),
+                child: Card(
+                    elevation: 12,
+                    shape: const RoundedRectangleBorder(),
+                    child: Column(
+                      children: [
+                        //Widget for gender selection
+                        GenderWidget(onChange: (genderVal) {})
+                      ],
+                    )))));
   }
 }
